be6366bdd6b5eca3874430f469ecdedd03c3dc23
PratadoYoutube <e.deanmb@gmail.com>
2025-04-12 14:35:42 -0300

    fix: remoção de arquivos desnecessários

--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -1,11 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // ⬅️ IMPORTANTE
+import { FormsModule } from '@angular/forms'; 
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-form',
   standalone: true,
-  imports: [CommonModule, FormsModule], // ⬅️ ADICIONE AQUI
+  imports: [CommonModule, FormsModule], 
   templateUrl: './search-form.component.html',
   styleUrl: './search-form.component.scss'
 })
