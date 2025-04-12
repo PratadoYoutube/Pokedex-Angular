02975f7fc09ad52b37d45c1c935b4cbf19f70e1f
PratadoYoutube <e.deanmb@gmail.com>
2025-04-12 12:09:40 -0300

    feat: Pokédex-Angular versão inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,68 @@
-import { Component } from '@angular/core';
-import { PokemonDisplayComponent } from "./components/pokemon-display/pokemon-display.component";
-import { SearchFormComponent } from "./components/search-form/search-form.component";
-import { NavigationButtonsComponent } from "./components/navigation-buttons/navigation-buttons.component";
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from './services/pokemon.service';
 import { CommonModule } from '@angular/common';
+import { PokemonDisplayComponent } from './components/pokemon-display/pokemon-display.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { NavigationButtonsComponent } from './components/navigation-buttons/navigation-buttons.component';
+import { PokedexContainerComponent } from './components/pokedex-container/pokedex-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, PokemonDisplayComponent, SearchFormComponent, NavigationButtonsComponent],
+  imports: [
+    CommonModule,
+    PokemonDisplayComponent,
+    SearchFormComponent,
+    NavigationButtonsComponent,
+    PokedexContainerComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  pokemonName: string = 'pikachu';
-  pokemonNumber: number = 25;
-  pokemonImageUrl: string = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png';
+export class AppComponent implements OnInit {
+  pokemonName = '';
+  pokemonNumber = 0;
+  pokemonImageUrl = '';
+  currentPokemonId: number = 1;
+
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit() {
+    this.fetchPokemon(this.currentPokemonId);
+  }
+
+  fetchPokemon(id: number | string) {
+    this.pokemonService.getPokemon(id).subscribe({
+      next: (data) => {
+        this.pokemonName = data.name;
+        this.pokemonNumber = data.id;
+        this.pokemonImageUrl =
+          data.sprites.versions['generation-v']['black-white'].animated.front_default ||
+          data.sprites.other['official-artwork'].front_default ||
+          data.sprites.front_default;
+
+        this.currentPokemonId = data.id;
+      },
+      error: (err) => {
+        console.error('Pokémon não encontrado:', err);
+        this.pokemonName = 'Não encontrado';
+        this.pokemonNumber = 0;
+        this.pokemonImageUrl = '';
+      }
+    });
+  }
 
   onSearchPokemon(term: string) {
-    console.log('Buscando por:', term);
-    // Aqui você pode fazer a chamada à API futuramente
-    // Por enquanto, só atualizar os dados manualmente para testes:
-    this.pokemonName = term.toLowerCase();
-    this.pokemonNumber = 999; // Exemplo fixo
-    this.pokemonImageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png'; // Exemplo fixo
+    this.fetchPokemon(term.toLowerCase());
+  }
+
+  onPrevPokemon() {
+    if (this.currentPokemonId > 1) {
+      this.fetchPokemon(this.currentPokemonId - 1);
+    }
+  }
+
+  onNextPokemon() {
+    this.fetchPokemon(this.currentPokemonId + 1);
   }
 }
---
+++ src/app/components/pokedex-container/pokedex-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pokedex-container',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pokedex-container.component.html',
+  styleUrls: ['./pokedex-container.component.scss'],
+})
+export class PokedexContainerComponent {}
